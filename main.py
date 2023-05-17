fae28cb9003b8a194916acccfef3dc7fbb36ab24
keenborder786 <21110290@lums.edu.pk>
2023-05-18 01:04:18 +0500

    [chore]: Price and Post Ago Formatting Features Added

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from slack_bot import Slackbot
 from upwork_bot import UpworkBot
+import time
 from config import (
     QUERY,
     SLACK_APP_TOKEN,CHANNEL_ID)
 slack_bot = Slackbot(SLACK_APP_TOKEN,CHANNEL_ID)
 upwork_bot = UpworkBot(QUERY)
-
-html_data = upwork_bot.get_data()
-jobs_data = upwork_bot.parse_data(html_data)
-formatted_response = upwork_bot.format_data(jobs_data)
-slack_bot.send_update(formatted_response)
+i = 1
+## TO-DO: 
+    ## Duration , Experience Level, Project Type
+    ## Caching ---> Removing (After 12 Hours Removal)- Wipe out Option
+    ## DB storage ---> Redis (Caching)
+while True:
+    print(f'Stress Testing Run: {i}')
+    html_data = upwork_bot.get_data()
+    jobs_data = upwork_bot.parse_data(html_data)
+    formatted_response = upwork_bot.format_data(jobs_data)
+    slack_bot.send_update(formatted_response)
+    time.sleep(1.5)
+    i+=1
--- upwork_bot/bot.py
+++ upwork_bot/bot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-
+from selenium.webdriver.chrome.options import Options
 
 class UpworkBot:
     def __init__(self,query):
@@ -11,7 +11,9 @@ class UpworkBot:
         
         
         """
-        driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=1,1")
+        driver = webdriver.Chrome(chrome_options = chrome_options)
         driver.get(f"https://www.upwork.com/nx/jobs/search/?q={self.query.replace(' ','%20')}&sort=recency")
         jobs_layout = driver.find_element(By.CSS_SELECTOR,"div.up-card-section > div:nth-child(1) > div:nth-child(2)")
         html_data = jobs_layout.get_attribute('outerHTML')
@@ -26,7 +28,10 @@ class UpworkBot:
         job_list =soup.find_all('section', attrs={'data-test': 'JobTile'})
         jobs_data = {}
         for job in job_list:
-            jobs_data[job.find('a')['href']] = job.text
+            job_link = job.find('a')
+            job_features = job.find('div' , attrs={'data-test': 'JobTileFeatures','class':'row'})
+            jobs_data[job_link['href']] = {'Title' : job_link.text}
+            jobs_data[job_link['href']]['Features'] = job_features.text
         return jobs_data
 
     def format_data(self,jobs_data):
@@ -37,5 +42,16 @@ class UpworkBot:
         
         """
         job_formatted_list = f"*Following are the new {self.query} jobs that have been posted* \n"
-        for job_link,job_text in jobs_data.items(): job_formatted_list += f" - https://www.upwork.com{job_link} \n"
+        for job_link in jobs_data: 
+            title,features = jobs_data[job_link]['Title'],jobs_data[job_link]['Features'].split('\n')
+            other_meta_features = features[-1].strip().split(' ')
+            job_formatted_list += f" - <https://www.upwork.com{job_link} | {title}> \n"
+            if 'Fixed-price' in features[0].strip(): 
+                posted_time = features[2].strip()
+                price = f"{features[0].strip()}-{features[3].strip()}"
+            else:
+                posted_time = ' '.join(other_meta_features[:other_meta_features.index('ago')+1]) 
+                price = features[0].strip()
+            job_formatted_list += f"    - Price: {price} \n"
+            job_formatted_list += f"    - Posted Time: {posted_time} \n"
         return job_formatted_list
