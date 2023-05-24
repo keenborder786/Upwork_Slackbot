96c43d730f98be3f53ef5d9eeab96f7e26ba726a
keenborder786 <21110290@lums.edu.pk>
2023-05-25 02:58:42 +0500

    [chore]: refactored the parse_data method

--- upwork_bot/bot.py
+++ upwork_bot/bot.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
+from fake_headers import Headers
 
 class UpworkBot:
     def __init__(self,query):
@@ -11,13 +15,23 @@ class UpworkBot:
         
         
         """
-        chrome_options = Options()
-        chrome_options.add_argument("--window-size=1,1")
-        driver = webdriver.Chrome(chrome_options = chrome_options)
+        options = Options()
+        header = Headers(
+            browser="chrome",  # Generate only Chrome UA
+            os="win",  # Generate only Windows platform
+            headers=False # generate misc headers
+        )
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        customUserAgent = header.generate()['User-Agent']
+        options.add_argument(f"user-agent={customUserAgent}") # head a fake-header to work in headless mode.
+        
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         driver.get(f"https://www.upwork.com/nx/jobs/search/?q={self.query.replace(' ','%20')}&sort=recency")
         jobs_layout = driver.find_element(By.CSS_SELECTOR,"div.up-card-section > div:nth-child(1) > div:nth-child(2)")
         html_data = jobs_layout.get_attribute('outerHTML')
-        driver.close()
+        driver.quit()
         return html_data
     def parse_data(self, html_data):
         """
@@ -30,8 +44,9 @@ class UpworkBot:
         for job in job_list:
             job_link = job.find('a')
             job_features = job.find('div' , attrs={'data-test': 'JobTileFeatures','class':'row'})
-            jobs_data[job_link['href']] = {'Title' : job_link.text}
-            jobs_data[job_link['href']]['Features'] = job_features.text
+            jobs_data.update({job_link['href']:
+                                {'Features' : job_features.text , 'Title' : job_link.text}}
+                            )
         return jobs_data
 
     def format_data(self,jobs_data):
@@ -43,15 +58,18 @@ class UpworkBot:
         """
         job_formatted_list = f"*Following are the new {self.query} jobs that have been posted* \n"
         for job_link in jobs_data: 
-            title,features = jobs_data[job_link]['Title'],jobs_data[job_link]['Features'].split('\n')
-            other_meta_features = features[-1].strip().split(' ')
-            job_formatted_list += f" - <https://www.upwork.com{job_link} | {title}> \n"
-            if 'Fixed-price' in features[0].strip(): 
-                posted_time = features[2].strip()
-                price = f"{features[0].strip()}-{features[3].strip()}"
-            else:
-                posted_time = ' '.join(other_meta_features[:other_meta_features.index('ago')+1]) 
-                price = features[0].strip()
-            job_formatted_list += f"    - Price: {price} \n"
-            job_formatted_list += f"    - Posted Time: {posted_time} \n"
+            title,features = jobs_data[job_link]['Title'],jobs_data[job_link]['Features']
+            features = list(filter(lambda x: x != '', features.strip().
+                                                      replace(':','').replace('\n','').
+                                                      lower().split(' ')))
+            job_formatted_list += f" - <https://www.upwork.com{job_link} | {title.strip()}> \n"
+            
+            # TODO Add features in a dictionary and make a final response by iterating over the items
+            price = f'Hourly: {features[1]}' if 'hourly' in features else f'Fixed: {features[6]}'
+            experience = f'Experience: {"-".join(features[-4:-2]).capitalize()}'\
+                if 'entry' in features else f'Experience: {features[-3].capitalize()}' 
+            posted = f'Posted: {" ".join(features[features.index("posted")+1:features.index("posted")+4])}'
+            job_formatted_list += f"    - {price} \n"
+            job_formatted_list += f"    - {experience} \n"
+            job_formatted_list += f"    - {posted} \n"
         return job_formatted_list
