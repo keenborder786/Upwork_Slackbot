d3b4e7c0d8e2e8e8080bd3eda485dc26709690c0
keenborder786 <21110290@lums.edu.pk>
2023-05-25 02:59:43 +0500

    [chore]: Added the ability to run the bot in headless mode

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ from config import (
 slack_bot = Slackbot(SLACK_APP_TOKEN,CHANNEL_ID)
 upwork_bot = UpworkBot(QUERY)
 i = 1
-## TO-DO: 
-    ## Duration , Experience Level, Project Type
-    ## Caching ---> Removing (After 12 Hours Removal)- Wipe out Option
-    ## DB storage ---> Redis (Caching)
+
 while True:
     print(f'Stress Testing Run: {i}')
     html_data = upwork_bot.get_data()
     jobs_data = upwork_bot.parse_data(html_data)
     formatted_response = upwork_bot.format_data(jobs_data)
     slack_bot.send_update(formatted_response)
-    time.sleep(1.5)
+    time.sleep(0.25)
     i+=1
